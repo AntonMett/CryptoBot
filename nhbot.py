7766bba5e99c77eb2c2aa1b18f9c0901faf5d71a
Anton Mett <antonmett@gmail.com>
2021-06-09 23:01:40 +0300

    Fixed some bugs

--- nhbot.py
+++ nhbot.py
@@ -13,7 +13,7 @@ public_api = nicehash.public_api(host)
 
 
 def available_currency(currency):
-    return (private_api.get_accounts_for_currency(currency).get('available'))
+    return float(private_api.get_accounts_for_currency(currency).get('available'))
 
 
 def my_orders(market='BTCUSDT', status='all'):
@@ -21,7 +21,7 @@ def my_orders(market='BTCUSDT', status='all'):
 
 
 def current_btcusdt_price():
-    return (public_api.get_current_price().get('BTCUSDT'))
+    return float(public_api.get_current_price().get('BTCUSDT'))
 
 
 def make_new_sell_order(multiply, btcusdt=current_btcusdt_price()):
@@ -38,7 +38,7 @@ def make_new_sell_order(multiply, btcusdt=current_btcusdt_price()):
 
 def make_new_buy_order(multiply, btcusdt=current_btcusdt_price()):
     price = multiply*btcusdt
-    quantity = float(available_currency('USDT'))/price
+    quantity = available_currency('USDT')/price
     print(f'Creating BUY order\nPrice: {price} USDT\nQuantity: {quantity} BTC')
     private_api.post_order('BTCUSDT', 'BUY', 'LIMIT', price, quantity)
     print('Thank you! Your order have been created!')
@@ -50,26 +50,23 @@ def make_new_buy_order(multiply, btcusdt=current_btcusdt_price()):
 def run_bot():
     while True:
 
-        try:
-            if not list(my_orders('BTCUSDT', 'open')):
-
-                if current_btcusdt_price() > minimal_BTC_sell_price:
+        if not list(my_orders('BTCUSDT', 'open')):
+            if available_currency('BTC') >= 0.0001:
+                if (current_btcusdt_price()) > minimal_BTC_sell_price:
                     make_new_sell_order(1.005)
                     time.sleep(2)
                 elif current_btcusdt_price() <= minimal_BTC_sell_price:
                     make_new_sell_order(1, minimal_BTC_sell_price)
                     time.sleep(2)
-                elif current_btcusdt_price() >= maximum_BTC_buy_price:
-                    make_new_buy_order(1, maximum_BTC_buy_price)
-                    time.sleep(2)
-                elif current_btcusdt_price() < maximum_BTC_buy_price:
+            elif available_currency('BTC') < 0.0001:
+                if current_btcusdt_price() > maximum_BTC_buy_price:
                     make_new_buy_order(0.995)
                     time.sleep(2)
-            else:
-                time.sleep(1)
-
-        except:
-            print('OOOPS SOMETHING WENT WRONG!')
+                elif current_btcusdt_price() <= maximum_BTC_buy_price:
+                    make_new_buy_order(1, maximum_BTC_buy_price)
+                    time.sleep(2)
+        else:
+            time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -80,5 +77,5 @@ if __name__ == "__main__":
     print(
         f'BTC  MINIMAL SELLING PRICE IS SET TO : {minimal_BTC_sell_price} USDT')
     print(
-        f'BTC  MINIMAL BUYING PRICE IS SET TO : {maximum_BTC_buy_price} USDT')
+        f'BTC  MAXIMAL BUYING PRICE IS SET TO : {maximum_BTC_buy_price} USDT')
     run_bot()
